2663de922da1a754b3f87940f2a8eef7f2f5abae
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-12-11 21:11:52 +0100

    Add EmuTOS 0.9.8 192k

--- memory.js
+++ memory.js
@@ -39,7 +39,7 @@ EstyJs.Memory = function (opts) {
     }
 
 
-    load_binary_resource('./tos.img');
+    load_binary_resource('./etos192us.img');
 
     self.readByte = function (addr) {
         //0 - 00CFFFFF max 14mb of ram
